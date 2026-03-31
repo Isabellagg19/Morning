94ac20f3302d654c3ba64056e76479c6aa062ef2
Isabellagg19 <isabellaguioc123@gmail.com>
2026-03-31 14:04:17 -0500

    added breakfast pictures

--- tips.js
+++ tips.js
@@ -1,7 +1,9 @@
 const quotes= [
-    "ccc",
-    "mmm",
-    "nnn"
+    "✨“Smile in the mirror. Do that every morning and you’ll start to see a big difference in your life.” —Yoko Ono✨",
+    "✨“Don’t count the days. Make the days count.” —Muhammad Ali✨",
+    "✨“Every morning, I wake up saying, 'I’m still alive, a miracle.' And so I keep on pushing.” —Jim Carrey✨",
+    "✨“In the morning, instead of saying to yourself, I got to wake up say I get to wake up!” —Erykah Badu✨",
+    "✨“Some people dream of success, while other people get up every morning and make it happen.” —Wayne Huizenga✨"
 ];
 
 const tipsbtn = document.getElementById("motivation-btn");
